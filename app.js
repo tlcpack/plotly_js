d580dbcc2717715861da106112a80d3903db3937
Taylor Cooke <tcooke@relias.com>
2021-03-30 14:02:10 -0400

    chart showing

---
+++ app.js
@@ -0,0 +1,29 @@
+// var rawData = [['2004', 1000, 400], ['2005', 1170, 460], ['2006', 660, 1120], ['2007', 1030, 540]];
+
+// const xAxis = rawData.map(x => x[0]);
+// const sales = rawData.map(s => s[1]);
+// const expenses = rawData.map(e => e[2]);
+
+// console.log(xAxis);
+// console.log(sales);
+// console.log(expenses);
+
+// var trace1 = {
+//     x: xAxis,
+//     y: sales,
+//     name: 'Sales',
+//     type: 'bar'
+// };
+
+// var trace2 = {
+//     x: xAxis,
+//     y: expenses,
+//     name: 'Expenses',
+//     type: 'bar'
+// };
+
+// var data = [trace1, trace2];
+
+// var layout = {barmode: 'group'};
+
+// Plotly.newPlot('myDiv', data, layout);
